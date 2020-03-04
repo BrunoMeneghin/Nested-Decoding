7e77c653fb0c9016877b12e0717e28d340b61f47
Bruno Meneghin <bruno.meneghin@meifacil.com>
2020-03-04 19:21:15 -0300

    Decodable

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+guard let url = URL(string: "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22") else {
+    fatalError("This is URL is invalid")
+}
+
+struct Weather: Decodable {
+    let main: String
+    let description: String
+}
+
+struct WeatherResponse: Decodable {
+    let weather: [Weather]
+}
+
+URLSession.shared.dataTask(with: url) { data, response, error in
+
+    guard let data = data, error == nil else {
+        return
+    }
+    
+    let result = try? JSONDecoder().decode(WeatherResponse.self, from: data)
+    
+    if let result = result {
+        print(result)
+        
+        result.weather.forEach {
+            print($0.main)
+            print($0.description)
+           
+        }
+    }
+    
+}.resume()
