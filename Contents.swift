b269de9d34c4b63cbca7767bfa46bd65a4842b35
Bruno Meneghin <46831352+BrunoMeneghin@users.noreply.github.com>
2020-03-04 19:26:49 -0300

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 guard let url = URL(string: "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22") else {
-    fatalError("This is URL is invalid")
+    fatalError("The URL is invalid")
 }
 
 struct Weather: Decodable {
